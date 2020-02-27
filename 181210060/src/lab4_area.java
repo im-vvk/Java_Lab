d6987e63532452f1a09d9393a1625ba25f145a27
“[Vivek <[imvvk7@gmail.com]>
2020-02-27 08:54:24 +0530

    "adding temp file"

--- 181210060/src/lab4_area.java
+++ 181210060/src/lab4_area.java
@@ -3,23 +3,23 @@ import java.util.*;
 public class lab4_area {
 
 //    float cal_area(float a){
-//        return a*a;
-//    }
-//    float cal_area(float a, float b){
-//        return a*b;
-//    }
-//    float cal_area(float a, float b, float c){
-//        float s = (a+b+c)/2;
-//        return (float) Math.sqrt(s*(s-a)*(s-b)*(s-c));
-//    }
+////        return a*a;
+////    }
+////    float cal_area(float a, float b){
+////        return a*b;
+////    }
+////    float cal_area(float a, float b, float c){
+////        float s = (a+b+c)/2;
+////        return (float) Math.sqrt(s*(s-a)*(s-b)*(s-c));
+////    }
 
     public static void main(String[] args) {
 //        lab4_area ob1 = new lab4_area();
-        Shape ob1 = new Shape(5);
+        Shape ofloatb1 = new Shape(5);
         Shape ob2 = new Shape(5, 4);
         Shape ob3 = new Shape(3, 4, 5);
-        System.out.println(ob1.cal_area());
-        System.out.println(ob2.cal_area());
+//        System.out.println(ob1.cal_area());
+//        System.out.println(ob2.cal_area());
         System.out.println(ob3.cal_area(3, 4, 5));
     }
 
